2ee93641841360b48b31ca67f3f3e0ecfc8e5723
Nicholas Stebbings <nickolarse@riseup.net>
2025-03-12 18:02:43 +1300

    feat(ui): Conditional rendering of Login/Logoff buttons and logoff handler

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts">
-import Login from './components/Login.vue'
-</script>
-
+<!-- src/App.vue -->
 <template>
   <nav>
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/bind-wallet">Bind Wallet</router-link>
+    <template v-if="!auth.user">
+      <router-link to="/login">Login</router-link>
+    </template>
+    <template v-else>
+      <router-link to="/bind-wallet">Bind Wallet</router-link> | 
+      <a href="#" @click.prevent="handleLogoff">Log Off</a>
+    </template>
   </nav>
   <router-view></router-view>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from './store/AuthStore'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+    const auth = useAuthStore()
+
+    const handleLogoff = async () => {
+      await auth.logoff()
+      router.replace('/login')
+    }
+
+    return {
+      auth,
+      handleLogoff
+    }
+  }
+})
+</script>
