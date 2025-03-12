2a1d5121bf23958b6e5bfa9fb29f8e313af5d584
Nicholas Stebbings <nickolarse@riseup.net>
2025-03-12 17:58:36 +1300

    feat(api): Add logoff route/handler

--- crates/lib/lib-web/src/handlers/handlers_login.rs
+++ crates/lib/lib-web/src/handlers/handlers_login.rs
@@ -1,6 +1,6 @@
 use crate::{
     error::{Error, Result},
-    token::set_token_cookie,
+    token::{remove_token_cookie, set_token_cookie},
 };
 use axum::{Json, Router, extract::State, routing::post};
 use lib_core::{
@@ -15,6 +15,8 @@ use serde::Deserialize;
 use serde_json::{Value, json};
 use tower_cookies::Cookies;
 use tracing::debug;
+
+// region:    --- Log In
 
 pub async fn api_login_handler(
     State(mm): State<ModelManager>,
@@ -52,7 +54,8 @@ pub async fn api_login_handler(
     set_token_cookie(&cookies, &user.username, &user.token_salt.to_string())?;
 
     Ok(Json(json!({
-        "success": true
+        "success": true,
+        "user_id": user.id
     })))
 }
 
@@ -61,3 +64,32 @@ pub struct LoginPayload {
     pub username: String,
     pub pwd: String,
 }
+
+// endregion: --- Log In
+
+// region:    --- Log Off
+
+/// Making this a json so that we get a pre-flight request
+#[derive(Debug, Deserialize)]
+pub struct LogoffPayload {
+    pub logoff: bool,
+}
+
+pub async fn api_logoff_handler(
+    State(mm): State<ModelManager>,
+    cookies: Cookies,
+    Json(payload): Json<LogoffPayload>,
+) -> Result<Json<Value>> {
+    debug!("{:<12} - api_logoff_handler", "HANDLER");
+
+    let should_logoff = payload.logoff;
+
+    if should_logoff {
+        remove_token_cookie(&cookies);
+    }
+
+    Ok(Json(json!({
+        "success": true
+    })))
+}
+// endregion: --- Log Off
--- crates/lib/lib-web/src/routes/routes_login.rs
+++ crates/lib/lib-web/src/routes/routes_login.rs
@@ -1,10 +1,11 @@
 use axum::{Router, routing::post};
 use lib_core::model::ModelManager;
 
-use crate::handlers::handlers_login::api_login_handler;
+use crate::handlers::handlers_login::{api_login_handler, api_logoff_handler};
 
 pub fn routes(mm: ModelManager) -> Router {
     Router::new()
         .route("/api/login", post(api_login_handler))
+        .route("/api/logoff", post(api_logoff_handler))
         .with_state(mm)
 }
